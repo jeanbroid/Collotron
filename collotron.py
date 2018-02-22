cb3a92651508734f892fe46c3d33cd3d49d935e8
Jean Bégaint <jean.begaint@irisa.fr>
2018-02-22 21:49:42 +0100

    #cleaning

--- collotron.py
+++ collotron.py
@@ -1,143 +1,149 @@
-#! /usr/bin/python
+"""
+This script works by loading a bunch of images in the current directory,
+cropping them using a clustering algorithm and placing patches randomly
+in order to create a "collage".
+The code is pretty straightforward to follow, don't hesitate to play
+with the parameters!
+"""
 
-#########
-# Intro #
-#########
-
-## Description
-# This script works by loading a bunch of images in the current directory,
-# cropping them using a clustering algorithm and placing patches randomly
-# in order to create a "collage".
-# The code is pretty straightforward to follow, don't hesitate to play
-# with the parameters!
-
-## Libraries
-from skimage import io, segmentation, data, transform
-import os, sys
-import fnmatch
-import numpy as np
+import os
 import random
+import sys
+
+from skimage import io, segmentation, transform, img_as_float, color
+import click
+import numpy as np
 
 
-#########
-# Tools #
-#########
+IMG_EXTENSIONS = ['.jpg', '.jpeg', '.png']
 
-## Add alpha layer to RGB image
-def add_alpha(img):
-	imga = np.zeros((img.shape[0], img.shape[1], 4), dtype=img.dtype)
-	imga[:,:,:3] = img[:,:,:3]
-	imga[:,:,3] = 1.0 if img.dtype == 'float64' else 255
-	return imga
 
-## Get axis aligned bounding box for pixels surrounded buy black pixels
-def get_aabb(img):
-	rows = np.any(img, axis=1)
-	cols = np.any(img, axis=0)
-	rmin, rmax = np.where(rows)[0][[0, -1]]
-	cmin, cmax = np.where(cols)[0][[0, -1]]
-	return img[rmin:rmax+1, cmin:cmax+1]
+def approx_equal(a, b, eps=1e-6):
+    return np.abs(a - b) < eps
 
-## Get list of patches from image found with SLIC
+
 def get_patches(img):
-	result = []
-	segmented = segmentation.slic(img, 
-								n_segments=10, 
-								compactness=25.0, 
-								max_iter=10, 
-								sigma=20, 
-								spacing=None, 
-								multichannel=True, 
-								convert2lab=True)
-	img = add_alpha(img)
+    """Get list of patches from image found with SLIC."""
+    patches = []
+    img_lab = color.rgb2lab(img)
+    segmented = segmentation.slic(img_lab,
+                                  n_segments=10,
+                                  compactness=25.0,
+                                  max_iter=10,
+                                  sigma=20,
+                                  convert2lab=False)
+    for i in range(np.max(segmented) + 1):
+        rows, cols = np.where(segmented == i)
+        data = img[rows, cols]
+        lab = np.mean(img_lab[rows, cols], axis=0)
+        patches.append((rows, cols, data, lab))
+    return patches
 
-	for i in range(np.amax(segmented)):
-		tmp = np.copy(img)
-		tmp[segmented[:] != i] = 0
-		cropped = get_aabb(tmp)
-		result.append(cropped)
-	return result
 
-## Resize while preserving aspect ratio, and fixing smallest dimension to 1000
-def resize(img):
-	ratio = float(img.shape[0]) / float(img.shape[1]) # old x/y
-	x = y = 0
-	if img.shape[0] <= img.shape[1]:
-		x = 1500
-		y = int(x/ratio)
-	else:
-		y = 1500
-		x = int(y*ratio)
-	return transform.resize(img, (x,y), cval=0)
+def resize(img, max_shape=(1280, 720)):
+    """Resize while preserving aspect ratio, and fixing smallest dimension to
+    `max_shape`."""
+    fx = max_shape[0] / img.shape[0]
+    fy = max_shape[1] / img.shape[1]
+    f = min(fx, fy)
+    if approx_equal(f, 1):
+        return img_as_float(img)
+    return transform.resize(img,
+                            (int(f * img.shape[0]), int(f * img.shape[1])),
+                            mode='constant', cval=0)
 
-## Paste patch to image specifying center position for patch, and alpha blend
+
 def paste(img, patch, pos):
-	width = patch.shape[0]
-	height = patch.shape[1]
-	xmin = max(pos[0]-width/2, 0)
-	xmax = min(pos[0]+width/2, img.shape[0])
-	ymin = max(pos[1]-height/2, 0)
-	ymax = min(pos[1]+height/2, img.shape[1])
-	difftoxmin = max(0, 0 - (pos[0]-width/2))
-	difftoxmax = max(0, (pos[0]+width/2)-(img.shape[0]) )
-	difftoymin = max(0, 0 - (pos[1]-height/2))
-	difftoymax = max(0, (pos[1]+height/2)-(img.shape[1]) )
-	for x in range(xmin,xmax):
-		for y in range(ymin,ymax):
-			if img[x,y,3] == 0:
-				img[x,y] = patch[x-xmin+difftoxmin,y-ymin+difftoymin]
-	#img[xmin:xmax, ymin:ymax] = patch[difftoxmin:2*(width/2)-difftoxmax,
-	#								  difftoymin:2*(height/2)-difftoymax]
-	return img
+    """Paste patch to image specifying center position for patch."""
+    rows, cols, data, _ = patch
+    height = np.ptp(rows)
+    width = np.ptp(cols)
+    xmin = np.min(cols)
+    ymin = np.min(rows)
+
+    for i, (x, y) in enumerate(zip(cols, rows)):
+        x = x - xmin + pos[1] - width // 2
+        if x < 0 or x > img.shape[1] - 1:
+            continue
+        y = y - ymin + pos[0] - height // 2
+        if y < 0 or y > img.shape[0] - 1:
+            continue
+        if img[y, x, 0] == -1:
+            img[y, x] = data[i]
 
 
-########
-# Main #
-########
+def load_images(directory):
+    """Return the list of images found in `directory`."""
+    paths = []
+    for filename in os.listdir(directory):
+        filepath = os.path.join(directory, filename)
+        if not os.path.isfile(filepath):
+            continue
+        _, ext = os.path.splitext(filepath)
+        if ext.lower() in IMG_EXTENSIONS:
+            paths.append(filepath)
+    if not paths:
+        return []
+    return io.imread_collection(paths)
 
-## Find and open images
-img_paths = []
-img_paths += fnmatch.filter(os.listdir('.'), '*.jpg')
-img_paths += fnmatch.filter(os.listdir('.'), '*.jpeg')
-img_paths += fnmatch.filter(os.listdir('.'), '*.png')
 
-images = io.imread_collection(img_paths)
+@click.command()
+@click.option('-d', '-directory',
+              type=click.Path(exists=True, file_okay=False),
+              help='Directory containing the images',
+              default='images',
+              show_default=True)
+@click.option('-r', '--reuse', is_flag=True, default=False,
+              help='Re-use already selected patches',
+              show_default=True)
+def main(**kwargs):
+    """Collotron: Automatic collage application."""
 
-## Resize images
-# note: this is done to harmonize the size of patches, 
-# although it is not mandatory
-images = [resize(img) for img in images]
-print 'Loaded {} images'.format(len(images))
+    print('Loading images...', end=' ')
+    sys.stdout.flush()
+    images = load_images(kwargs['directory'])
+    if not images:
+        print()
+        print('Error: no images found')
+        sys.exit(1)
+    print('done ({:d} images loaded)'.format(len(images)))
 
-## Extract patches
-# Clusters are found using kmeans which outputs masks
-# which are used to extract and crop clusters on transparent background
-#img = images[0]
-#patches = get_patches(img)
-patches = [p for img in images for p in get_patches(img)]
-print 'Extracted {} patches'.format(len(patches))
+    print('Resizing images...', end=' ')
+    sys.stdout.flush()
+    images = [resize(img) for img in images]
+    print('done')
 
-## Place patches
-# > select a random patch
-# > place it at a random location
-# > select a new random patch
-# > place it somewhere that's still not filled
-# > repeat until everything is filled
-while 1:
-	collage = np.zeros((1000, 1000, 4))
-	while np.any(collage[:,:,3]==0):
-		# select patch
-		patch = random.choice(patches)
-		# place it
-		empty_pixels = np.where(collage[:,:,3]==0)
-		position = [random.choice(empty_pixels[0]), 
-					random.choice(empty_pixels[1])]
-		print " > Pasting patch " + str(patches.index(patch)) \
-			+ " at " + str(position)
-		collage = paste(collage, patch, position)
+    print('Extracting patches...', end=' ')
+    sys.stdout.flush()
+    patches = [p for img in images for p in get_patches(img)]
+    print('done ({} patches extracted)'.format(len(patches)))
 
-	print "Over! File written to collage.jpg"
-	io.imsave("collage.jpg", collage)
-	io.imshow(collage)
-	io.show()
+    width = 1280
+    height = 1280
+    collage = np.ones((height, width, 3))
+    collage[:] = -1
+    while 1:
+        if not kwargs['reuse']:
+            if not patches:
+                break
+        patch_idx, patch = random.choice(list(enumerate(patches)))
+        if not kwargs['reuse']:
+            patches.pop(patch_idx)
+        rows, cols = np.where(collage[:, :, 0] == -1)
+        if rows.size == 0:
+            break
+        idx = random.choice(list(enumerate(cols)))[0]
+        position = (rows[idx], cols[idx])
+        print(' > Pasting patch {:d} at {:d}x{:d}'.format(
+            patch_idx, position[0], position[1]))
+        paste(collage, patch, position)
 
+    collage[collage == -1] = 0
+    print('Over! File written to collage.jpg')
+    io.imsave('collage.jpg', collage)
+    io.imshow(collage)
+    io.show()
+
+
+if __name__ == "__main__":
+    main()
